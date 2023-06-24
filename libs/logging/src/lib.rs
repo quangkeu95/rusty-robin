f2882bbe8c05021ea7972457ded4f5e662404e37
Nguyen Le Quang <quangkeu95@gmail.com>
2023-06-24 12:56:08 +0700

    feat(logging): add logging package

---
+++ libs/logging/src/lib.rs
@@ -0,0 +1,32 @@
+/// Reexport
+pub use tracing::*;
+
+use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
+
+#[derive(Debug)]
+pub enum LoggingFormat {
+    Json,
+    Plain,
+}
+
+pub fn init_tracing(logging_format: Option<LoggingFormat>) {
+    // let (non_blocking_writer, _guard) = tracing_appender::non_blocking(std::io::stdout());
+    // let formatting_layer = BunyanFormattingLayer::new(app_name.to_owned(), non_blocking_writer);
+    let env_layer = EnvFilter::from_default_env();
+
+    let timer = tracing_subscriber::fmt::time::UtcTime::rfc_3339();
+    let logging_format = logging_format.unwrap_or(LoggingFormat::Plain);
+    if let LoggingFormat::Json = logging_format {
+        tracing_subscriber::registry()
+            // .with(opentelemetry)
+            .with(fmt::Layer::default().with_timer(timer).json())
+            .with(env_layer)
+            .init();
+    } else {
+        tracing_subscriber::registry()
+            // .with(opentelemetry)
+            .with(fmt::Layer::default().with_timer(timer))
+            .with(env_layer)
+            .init();
+    }
+}
